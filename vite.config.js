04dba0e43c28f7149e2cd63301983220d147fd00
raja owais <rajaowais@rajas-MacBook-Air.local>
2025-03-29 12:22:45 +0500

    fifteen  commit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist',
     rollupOptions: {
-      external: ['react-router-dom']
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom']
+        }
+      }
     }
+  },
+  server: {
+    historyApiFallback: true
   }
-})
+});
