32a48fc1dbfbade0f514be22dd66c89aedd7a38a
raja owais <rajaowais@rajas-MacBook-Air.local>
2025-03-29 05:13:22 +0500

    fourth  commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './app.css'
+import './app.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import New from './pages/New';
